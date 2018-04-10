4c9d13bedff9735499c7edf2c62f8bb9b79bbd02
Mathias <mathiasjlarsen@gmail.com>
2018-04-10 22:11:07 +0200

    Spawning game in Unity

---
+++ ItemPickUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : Interactables
+{
+    public Item item;
+    CharacterStats charStats;
+
+    public override void Interact()
+    {
+        base.Interact();
+        PickUp();
+    }
+
+    void PickUp()
+    {
+        item.Use();
+        Destroy(gameObject);
+    }
+
+}
